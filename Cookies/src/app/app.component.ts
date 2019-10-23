dc5d41d4c374175b7bcc2419b918bcc7009b1308
Pavel <pahan.rolich@mail.ru>
2019-10-23 15:43:09 +0300

    beta project

--- Cookies/src/app/app.component.ts
+++ Cookies/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cookies';
+  buttons = [
+    { text: 'Sweet... cookies!', alt: '', colorButton: '#3498db', colorText: '#ffffff', position: 1, type: 'notTricky' },
+    { text: 'No. Not for me!', alt: 'Don\'t click me', colorButton: '#ffffff', colorText: '#3498db', position: 2, type: 'tricky' }
+  ];
+
+  onMouseover(event: any, type: string) {
+    if (type === 'tricky') {
+      let rand = Math.floor(Math.random() * 4) + 1;
+      while (this.buttons[1].position === rand || rand > 4 || rand < 1) {
+        rand = Math.floor(Math.random() * 4) + 1;
+      }
+      this.buttons[0].position = this.buttons[1].position;
+      this.buttons[1].position = rand;
+    }
+  }
+
 }
--- Cookies/src/app/app.module.ts
+++ Cookies/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ButtonComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
